4791339499dac053681881a9c3cd7cdc0a0d222b
Emma Strickland <emlstrick@gmail.com>
2021-07-06 15:52:57 -0400

    Redesign website

---
+++ packages/api/server.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+const cors = require('cors');
+const express = require('express');
+
+const PORT = 4001;
+
+const app = express();
+app.use(cors());
+
+// API key
+const LASTFM_API_KEY = "59aae518bb2169ec018f2706d6a0d135"
+// API base URL
+const LASTFM_BASE_URL = "http://ws.audioscrobbler.com/2.0/"
+
+app.get("/recentlyPlayed", (_req, res) => {
+    axios.get(`${LASTFM_BASE_URL}?method=user.getrecenttracks&user=stricklandemma&api_key=${LASTFM_API_KEY}&format=json`)
+        .then(response => {
+            const recentMusic = response.data.recenttracks.track[0];
+            const image = recentMusic.image && recentMusic.image.length > 0 ? recentMusic.image[2]['#text'] : undefined;
+            console.log('response: ', response.data);
+            res.json({
+                track: recentMusic.name,
+                artist: recentMusic.artist['#text'],
+                album: recentMusic.album['#text'],
+                image: image
+            })
+        })
+        .catch(error => {
+            console.log('Lastfm error: ', error);
+            res.status(500).json({
+                error: {
+                    message: "Error fetching lastfm api"
+                }
+            })
+        });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
