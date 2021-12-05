a6344c6ecf096221306be4b5d6e5d25072116f55
Emma Strickland <emlstrick@gmail.com>
2021-12-05 17:44:20 -0500

    Add isLoading for spotify info

--- packages/app/src/App.js
+++ packages/app/src/App.js
@@ -1,58 +1,66 @@
-import React/*, { useEffect, useState } */ from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import config from './config';
 
 function App() {
-  // const [response, setResponse] = useState('');
+  const [response, setResponse] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-  // const openSpotifyProfile = () => {
-  //   window.open(config.SPOTIFY_PROFILE_URL, "_blank");
-  // }
+  const openSpotifyProfile = () => {
+    window.open(config.SPOTIFY_PROFILE_URL, "_blank");
+  }
 
-  // useEffect(() => {
-  //   fetch(`${config.API_BASE_URL}/recentlyPlayed`)
-  //     .then(res => res.json())
-  //     .then(result => {
-  //       setResponse(result);
-  //     }).catch(error => {
-  //       console.log('error: ', error);
-  //     });
-  // }, []);
+  useEffect(() => {
+    fetch(`${config.API_BASE_URL}/recentlyPlayed`)
+      .then(res => res.json())
+      .then(result => {
+        setResponse(result);
+        setIsLoading(true)
+      }).catch(error => {
+        console.log('error: ', error);
+      });
+  }, []);
 
   return (
     <div className="App">
-      {/* {response && ( */}
-      <div className="container">
-        <div className="title">{config.NAME}</div>
-        <div className="subtext">
-          Software Engineer based in Brooklyn, NY
-          <hr />
-          <div className="link">
-            <a href={`mailto:${config.EMAIL_ADDRESS}`} target="_blank" rel="noopener noreferrer">
-              Email
-            </a>
+      {response && (
+        <div className="container">
+          <div className="title">{config.NAME}</div>
+          <div className="subtext">
+            Software Engineer based in Brooklyn, NY
+            <hr />
+            <div className="link">
+              <a href={`mailto:${config.EMAIL_ADDRESS}`} target="_blank" rel="noopener noreferrer">
+                Email
+              </a>
+            </div>
+            <div className="link">
+              <a href={config.LINKEDIN_URL} target="_blank" rel="noopener noreferrer">
+                LinkedIn
+              </a>
+            </div>
+            <div className="link">
+              <a href={config.GITHUB_URL} target="_blank" rel="noopener noreferrer">
+                Github
+              </a>
+            </div>
+            <hr />
+            <div className="current">
+              Currently listening to:
+            </div>
+
+            {isLoading ?
+              <div className="music-text">
+                <img src={response.image} alt="album-art" className="album-art" onClick={openSpotifyProfile} />
+                <div className="music-subtitle">"{response.track}" – {response.artist}</div>
+              </div>
+              :
+              <div>Loading...</div>
+            }
+
           </div>
-          <div className="link">
-            <a href={config.LINKEDIN_URL} target="_blank" rel="noopener noreferrer">
-              LinkedIn
-            </a>
-          </div>
-          <div className="link">
-            <a href={config.GITHUB_URL} target="_blank" rel="noopener noreferrer">
-              Github
-            </a>
-          </div>
-          <hr />
-          <div className="current">
-            Currently listening to:
-          </div>
-          {/* <div className="music-text">
-            <img src={response.image} alt="album-art" className="album-art" onClick={openSpotifyProfile} />
-            <div className="music-subtitle">"{response.track}" – {response.artist}</div>
-          </div> */}
         </div>
-      </div>
-      {/* )} */}
+      )}
     </div>
   );
 }
