20229a9460f12dadd91b60a2b2641ce41355ce43
Emma Strickland <emlstrick@gmail.com>
2021-12-16 17:17:23 -0500

    Fix rendering bug

--- packages/api/server.js
+++ packages/api/server.js
@@ -14,29 +14,29 @@ app.use(cors());
 const LASTFM_BASE_URL = "http://ws.audioscrobbler.com/2.0/"
 
 app.get("/recentlyPlayed", (_req, res) => {
-    axios.get(`${LASTFM_BASE_URL}?method=user.getrecenttracks&user=stricklandemma&api_key=${process.env.LASTFM_API_KEY}&format=json`)
-        .then(response => {
-            const recentMusic = response.data.recenttracks.track[0];
-            const image = recentMusic.image && recentMusic.image.length > 0 ? recentMusic.image[2]['#text'] : undefined;
-            console.log('response: ', response.data);
-            res.json({
-                track: recentMusic.name,
-                artist: recentMusic.artist['#text'],
-                album: recentMusic.album['#text'],
-                image: image
-            })
-        })
-        .catch(error => {
-            console.log('Lastfm error: ', error);
-            res.status(500).json({
-                error: {
-                    message: "Error fetching lastfm api"
-                }
-            })
-        });
+  axios.get(`${LASTFM_BASE_URL}?method=user.getrecenttracks&user=stricklandemma&api_key=${process.env.LASTFM_API_KEY}&format=json`)
+    .then(response => {
+      const recentMusic = response.data.recenttracks.track[0];
+      const image = recentMusic.image && recentMusic.image.length > 0 ? recentMusic.image[2]['#text'] : undefined;
+      console.log('response: ', response.data);
+      res.json({
+        track: recentMusic.name,
+        artist: recentMusic.artist['#text'],
+        album: recentMusic.album['#text'],
+        image: image
+      })
+    })
+    .catch(error => {
+      console.log('Lastfm error: ', error);
+      res.status(500).json({
+        error: {
+          message: "Error fetching lastfm api"
+        }
+      })
+    });
 });
 
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
--- packages/app/src/App.js
+++ packages/app/src/App.js
@@ -4,7 +4,7 @@ import config from './config';
 
 function App() {
   const [response, setResponse] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const openSpotifyProfile = () => {
     window.open(config.SPOTIFY_PROFILE_URL, "_blank");
@@ -15,7 +15,7 @@ function App() {
       .then(res => res.json())
       .then(result => {
         setResponse(result);
-        setIsLoading(true)
+        setIsLoading(false)
       }).catch(error => {
         console.log('error: ', error);
       });
@@ -23,44 +23,43 @@ function App() {
 
   return (
     <div className="App">
-      {response && (
-        <div className="container">
-          <div className="title">{config.NAME}</div>
-          <div className="subtext">
-            Software Engineer based in Brooklyn, NY
-            <hr />
-            <div className="link">
-              <a href={`mailto:${config.EMAIL_ADDRESS}`} target="_blank" rel="noopener noreferrer">
-                Email
-              </a>
-            </div>
-            <div className="link">
-              <a href={config.LINKEDIN_URL} target="_blank" rel="noopener noreferrer">
-                LinkedIn
-              </a>
-            </div>
-            <div className="link">
-              <a href={config.GITHUB_URL} target="_blank" rel="noopener noreferrer">
-                Github
-              </a>
-            </div>
-            <hr />
-            <div className="current">
-              Currently listening to:
-            </div>
-
-            {isLoading ?
-              <div className="music-text">
-                <img src={response.image} alt="album-art" className="album-art" onClick={openSpotifyProfile} />
-                <div className="music-subtitle">"{response.track}" – {response.artist}</div>
-              </div>
-              :
-              <div>Loading...</div>
-            }
-
+      {/* {response && ( */}
+      <div className="container">
+        <div className="title">{config.NAME}</div>
+        <div className="subtext">
+          Software Engineer based in Brooklyn, NY
+          <hr />
+          <div className="link">
+            <a href={`mailto:${config.EMAIL_ADDRESS}`} target="_blank" rel="noopener noreferrer">
+              Email
+            </a>
           </div>
+          <div className="link">
+            <a href={config.LINKEDIN_URL} target="_blank" rel="noopener noreferrer">
+              LinkedIn
+            </a>
+          </div>
+          <div className="link">
+            <a href={config.GITHUB_URL} target="_blank" rel="noopener noreferrer">
+              Github
+            </a>
+          </div>
+          <hr />
+          <div className="current">
+            Currently listening to:
+          </div>
+
+          {!isLoading ?
+            <div className="music-text">
+              <img src={response.image} alt="album-art" className="album-art" onClick={openSpotifyProfile} />
+              <div className="music-subtitle">"{response.track}" – {response.artist}</div>
+            </div> :
+            <div className="current">Loading...</div>
+          }
+
         </div>
-      )}
+      </div>
+      {/* )} */}
     </div>
   );
 }
